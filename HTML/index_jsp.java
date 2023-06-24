516085e0bc71791750fb136cbc9fbfd808a7a510
Subathra2801 <102725024+Subathra2801@users.noreply.github.com>
2023-06-24 21:07:15 +0530

    Add files via upload

---
+++ HTML/index_jsp.java
@@ -0,0 +1,154 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
+      out.write("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\" />\n");
+      out.write("        <title>Novel Data Hiding</title>\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n");
+      out.write("        <!--[if IE 6]>\n");
+      out.write("                <script type=\"text/javascript\" src=\"unitpngfix.js\"></script>\n");
+      out.write("        <![endif]--> \n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <div class=\"wrap\">\n");
+      out.write("            <div class=\"header\">\n");
+      out.write("                <div class=\"logo\">\n");
+      out.write("                    <a href=\"index.html\"><img src=\"images/logo.gif\" alt=\"\" title=\"\" border=\"0\" />\n");
+      out.write("                    </a>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("\n");
+      out.write("            <div id=\"menu\">\n");
+      out.write("                <ul>                                                                       \n");
+      out.write("                    <li class=\"selected\"><a href=\"index.jsp\">Home</a></li>\n");
+      out.write("                    <li><a href=\"AdminLogin.jsp\">Admin Login</a></li>\n");
+      out.write("                    <li><a href=\"UserLogin.jsp\">User Login</a></li>\n");
+      out.write("                    <li><a href=\"About.jsp\">About Us</a></li>\n");
+      out.write("                    <li><a href=\"Contact.jsp\">Contact Us</a></li>\n");
+      out.write("                </ul>\n");
+      out.write("            </div> \n");
+      out.write("\n");
+      out.write("            <div class=\"tab_bg\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                <div class=\"tab_left\">\n");
+      out.write("                    <img src=\"images/big_icon.png\" alt=\"\" title=\"\" border=\"0\" class=\"big_icon\" />       \n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("                <div class=\"middle_text\">\n");
+      out.write("                    <h1 style=\"color: white;\">A Novel Data Hiding Algorithm for High Dynamic Range Images</h1>\n");
+      out.write("                    <p>\n");
+      out.write("                        \n");
+      out.write("                    </p>\n");
+      out.write("\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("                <div class=\"tabs\">\n");
+      out.write("                    <img src=\"images/cs1.jpg\" style=\"height:200px; width: 220px;\"/>\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                <div class=\"clear\"></div>\n");
+      out.write("            </div>\n");
+      out.write("\n");
+      out.write("            <div class=\"center_content\">\n");
+      out.write("                <div class=\"left_content\">\n");
+      out.write("                    <div class=\"title\">Main Services</div>\n");
+      out.write("                    <p>\n");
+      out.write("                        Data hiding, also known as data embedding, is a method of using  digital  media  to  conceal  critical  messages.  In general, the object in which secret messages are intended to  be embedded is referred to as the cover medium, and the object in  which  the  messages  are  concealed  is  called  the  stego medium. An image data hiding technique is usually evaluated in terms of the embedding capacity, also known as the payload, and the quality of the stego image.\n");
+      out.write("                    </p>\n");
+      out.write("\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("                <div class=\"right_content\">\n");
+      out.write("                    <div class=\"title\">Testimonials</div>\n");
+      out.write("\n");
+      out.write("                    <div class=\"testimonials\">\n");
+      out.write("                        <p>\n");
+      out.write("                             The  dynamic range of a scene is the contrast ratio between its brightest and \n");
+      out.write("darkest parts. HDR images represent a large range of luminance using  floating-point  numbers.  This  is  in  contrast  to  low dynamic range (LDR) images which represent  a  limited range of  luminance  using  integers.  A  set  of  advanced  image techniques allowing a far greater dynamic range of exposures than  normal  digital  image  techniques  has  been  investigated herein.\n");
+      out.write("                        </p>\n");
+      out.write("                    </div>\n");
+      out.write("                    \n");
+      out.write("\n");
+      out.write("                    \n");
+      out.write("\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("                <div class=\"clear\"></div>   \n");
+      out.write("            </div>\n");
+      out.write("\n");
+      out.write("            <div id=\"footer\">\n");
+      out.write("                <div class=\"footer_links\">                      \n");
+      out.write("                    <a href=\"index.jsp\">home</a>\n");
+      out.write("                    <a href=\"AdminLogin.jsp\">Admin Login</a>\n");
+      out.write("                    <a href=\"UserLogin.jsp\">User Login</a>\n");
+      out.write("                    <a href=\"About.jsp\">About</a>\n");
+      out.write("                    <a href=\"Contact.jsp\">Contact Us</a>\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("                <div class=\"copyright\">\n");
+      out.write("                    <a href=\"http://csstemplatesmarket.com\"> &copy;Copy rights by Nias & Aravind</a>\n");
+      out.write("                </div>    \n");
+      out.write("\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
